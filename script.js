591c138dff0372b871d49f499e147cdf918bfedf
GalinaGalaxy <galina30911@gmail.com>
2025-05-26 21:23:29 +0300

    Реализовано добавление привычки

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log ('JavaScript работает!')
+const input = document.getElementById('habit-input');
+const addBtn = document.getElementById('add-habit-btn');
+const habitsList = document.getElementById('habits-list');
+
+addBtn.addEventListener('click', function(){
+  const habit = input.value.trim()
+  if (habit !== '') {
+    addHabit(habit);
+    input.value = '';
+  }
+});
+
+function addHabit(habitName) {
+  const li = document.createElement('li');
+  li.className = 'habit-item';
+  li.innerHTML = `
+  <span>${habitName}</span>
+  <button class='done-btn>Выполнено</button>`;
+  habitsList.appendChild(li)
+}
